d051292f7171f8e3411898d2c783bb437960e579
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-01-06 21:11:44 +0100

    Add benchmarks

---
+++ CrossValidation.Benchmarks/Program.cs
@@ -0,0 +1,12 @@
+﻿using BenchmarkDotNet.Columns;
+using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Loggers;
+using BenchmarkDotNet.Running;
+using BenchmarkDotNet.Validators;
+
+var configuration = new ManualConfig()
+    .WithOptions(ConfigOptions.DisableOptimizationsValidator)
+    .AddValidator(JitOptimizationsValidator.DontFailOnError)
+    .AddLogger(ConsoleLogger.Default)
+    .AddColumnProvider(DefaultColumnProviders.Instance);
+BenchmarkRunner.Run(typeof(Program).Assembly, configuration);
---
+++ CrossValidation.Benchmarks/WithErrorBenchmarks.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using CrossValidation.Errors;
+using CrossValidation.Rules;
+
+namespace CrossValidation.Benchmarks;
+
+[MemoryDiagnoser]
+[HtmlExporter]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+[RankColumn]
+public class WithErrorBenchmarks
+{
+    private IRule<int> _rule = default!;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _rule = Validate.That(1);
+    }
+    
+    [Benchmark(Baseline = true)]
+    public void WithError()
+    {
+        _rule
+            .WithError(new CrossValidationError(Message: "Error message"))
+            .Must(_ => true);
+    }
+}
