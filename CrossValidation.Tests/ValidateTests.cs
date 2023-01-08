67cc4ad6c844c122317028c21e97aa9a5e676ea0
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-01-08 23:24:33 +0100

    Add Validate.Is

---
+++ CrossValidation.Tests/ValidateTests.cs
@@ -0,0 +1,56 @@
+﻿using CrossValidation.Errors;
+using CrossValidation.Extensions;
+using CrossValidation.Resources;
+using Shouldly;
+using Xunit;
+
+namespace CrossValidation.Tests;
+
+public class ValidateTests
+{
+    [Fact]
+    public void ValidateIs()
+    {
+        var action = () => Validate.Is(false);
+
+        action.ShouldThrowValidationError();
+    }
+
+    [Fact]
+    public void ValidateIs_with_error()
+    {
+        var expectedCode = nameof(ErrorResource.NotNull);
+        var expectedDetails = "Details";
+        var errorForValidation = new CrossValidationError
+        {
+            Code = expectedCode,
+            Details = expectedDetails
+        };
+
+        var action = () => Validate.Is(false, errorForValidation);
+
+        var error = action.ShouldThrowValidationError();
+        error.Code.ShouldBe(expectedCode);
+        error.Message.ShouldBe(ErrorResource.NotNull);
+        error.Details.ShouldBe(expectedDetails);
+    }
+
+    [Fact]
+    public void ValidateIs_with_raw_customizations()
+    {
+        var expectedCode = "Expected code";
+        var expectedMessage = "Expected message";
+        var expectedDetails = "Expected details";
+
+        var action = () => Validate.Is(
+            false,
+            expectedCode,
+            message: expectedMessage,
+            details: expectedDetails);
+
+        var error = action.ShouldThrowValidationError();
+        error.Code.ShouldBe(expectedCode);
+        error.Message.ShouldBe(expectedMessage);
+        error.Details.ShouldBe(expectedDetails);
+    }
+}
--- CrossValidation/Validate.cs
+++ CrossValidation/Validate.cs
@@ -1,5 +1,6 @@
 ﻿using System.Diagnostics.Contracts;
 using System.Linq.Expressions;
+using CrossValidation.Errors;
 using CrossValidation.Rules;
 
 namespace CrossValidation;
@@ -18,5 +19,63 @@ public static class Validate
         Expression<Func<TModel, TField>> fieldSelector)
     {
         return IValidRule<TField>.CreateFromFieldSelector(model, fieldSelector);
+    }
+
+    public static void Is(bool condition)
+    {
+        InternalIs(condition);
+    }
+
+    public static void Is(bool condition, CrossValidationError error)
+    {
+        InternalIs(condition, error: error);
+    }
+    
+    public static void Is(
+        bool condition,
+        string code,
+        string? message = null,
+        string? details = null)
+    {
+        InternalIs(
+            condition,
+            code: code,
+            message: message,
+            details: details);
+    }
+
+    private static void InternalIs(
+        bool condition,
+        CrossValidationError? error = null,
+        string? code = null,
+        string? message = null,
+        string? details = null)
+    {
+        if (!condition)
+        {
+            var rule = IValidRule<bool>.CreateFromField(condition);
+
+            if (error is not null)
+            {
+                rule = rule.WithError(error);
+            }
+            
+            if (code is not null)
+            {
+                rule = rule.WithCode(code);
+            }
+            
+            if (message is not null)
+            {
+                rule = rule.WithMessage(message);
+            }
+            
+            if (details is not null)
+            {
+                rule = rule.WithDetails(details);
+            }
+
+            rule.Must(_ => false);
+        }
     }
 }
