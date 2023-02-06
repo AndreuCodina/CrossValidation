37f8de344eb4e0c86267d4886f8be34a9bac61e3
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-02-07 00:24:03 +0100

    Update middleware

--- CrossValidation/DependencyInjection/CrossValidationMiddleware.cs
+++ CrossValidation/DependencyInjection/CrossValidationMiddleware.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using System.Diagnostics;
+using System.Net;
 using System.Text.Json;
 using CrossValidation.Exceptions;
 using CrossValidation.Utils;
@@ -36,30 +37,52 @@ internal class CrossValidationMiddleware : IMiddleware
         var problemDetails = new CrossProblemDetails();
         var genericError = "An error occurred";
         var httpStatusCode = HttpStatusCode.InternalServerError;
+        string? title = null;
+        string? details = null;
 
-        if (exception is ValidationException or ValidationListException)
+        if (exception is ValidationException or ValidationException)
         {
             httpStatusCode = HttpStatusCode.UnprocessableEntity;
-            problemDetails.Title = "One or more validation errors occurred";
+            title = "One validation errors occurred";
+        }
+        else if (exception is ValidationException or ValidationListException)
+        {
+            httpStatusCode = HttpStatusCode.UnprocessableEntity;
+            title = "Several validation errors occurred";
+        }
+        else if (exception is EnsureException {Error: ModelNullabilityValidatorError error})
+        {
+            httpStatusCode = HttpStatusCode.BadRequest;
+            title = "Nullability error";
+
+            if (error is ModelNullabilityValidatorError.NonNullablePropertyIsNullError err)
+            {
+                details = err.ToString();
+            }
+            else if (error is ModelNullabilityValidatorError.NonNullablePropertyIsNullError errr)
+            {
+                details = errr.ToString();
+            }
+            else
+            {
+                throw new UnreachableException();
+            }
         }
         else if (exception is EnsureException ensureException)
         {
             httpStatusCode = HttpStatusCode.UnprocessableEntity;
-            problemDetails.Title = genericError;
-        }
-        else if (exception is EnsureException {Error: ModelNullabilityValidatorError})
-        {
-            httpStatusCode = HttpStatusCode.BadRequest;
-            problemDetails.Title = "Nullability error";
+            title = genericError;
         }
         else
         {
             _logger.LogError(exception, exception.Message);
-            problemDetails.Title = genericError;
+            title = genericError;
             problemDetails.Detail = _environment.IsDevelopment() ? exception.Message : null;
         }
 
         problemDetails.Status = (int)httpStatusCode;
+        problemDetails.Title = title;
+        problemDetails.Detail = details;
         var response = JsonSerializer.Serialize(problemDetails);
         context.Response.StatusCode = problemDetails.Status.Value;
         context.Response.ContentType = "application/problem+json";
