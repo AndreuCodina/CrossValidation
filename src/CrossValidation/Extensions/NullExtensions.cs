833e16b8be84b72f8dd1ba611a9ade0611126564
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-02-24 13:54:28 +0100

    Update CI

--- src/CrossValidation/Extensions/NullExtensions.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CrossValidation.Extensions;
-
-public static class NullExtensions
-{
-    public static TResult? Map<T, TResult>(this T? value, Func<T, TResult> f)
-    {
-        return value is not null ? f(value) : default;
-    }
-}
