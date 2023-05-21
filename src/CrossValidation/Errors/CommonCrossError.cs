592fdd154baac13db9962eaaa13a3260779e8f4f
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-05-21 21:12:12 +0200

    Clean code

--- src/CrossValidation/Errors/CommonCrossError.cs
+++ src/CrossValidation/Errors/CommonCrossError.cs
@@ -11,40 +11,19 @@ public record CommonCrossError(string Code) : CodeCrossError(Code)
     public record Null() : CommonCrossError(nameof(ErrorResource.Null));
 
     public record GreaterThan<TField>(TField ComparisonValue) :
-        CommonCrossError(nameof(ErrorResource.GreaterThan))
-    {
-        // public override void AddPlaceholderValues()
-        // {
-        //     AddPlaceholderValue(ComparisonValue);
-        // }
-    }
+        CommonCrossError(nameof(ErrorResource.GreaterThan));
 
     public record Enum() : CommonCrossError(nameof(ErrorResource.Enum));
     
     public record EnumRange() : CommonCrossError(nameof(ErrorResource.Enum));
-    
+
     public record LengthRange(int Minimum, int Maximum, int TotalLength) :
         CommonCrossError(nameof(ErrorResource.LengthRange)),
-        ILengthError
-    {
-        public override void AddPlaceholderValues()
-        {
-            AddPlaceholderValue(Minimum);
-            AddPlaceholderValue(Maximum);
-            AddPlaceholderValue(TotalLength);
-        }
-    }
-    
+        ILengthError;
+
     public record MinimumLength(int Minimum, int TotalLength) :
         CommonCrossError(nameof(ErrorResource.MinimumLength)),
-        ILengthError
-    {
-        public override void AddPlaceholderValues()
-        {
-            AddPlaceholderValue(Minimum);
-            AddPlaceholderValue(TotalLength);
-        }
-    }
+        ILengthError;
     
     public record Predicate() : CommonCrossError(nameof(ErrorResource.General));
     
