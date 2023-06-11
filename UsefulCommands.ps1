6e739fd920a77dfe60e72a244620a43bc7549850
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-06-11 22:25:32 +0200

    Update UsefulCommands

--- UsefulCommands.ps1
+++ UsefulCommands.ps1
@@ -1,6 +1,6 @@
 # dotnet tool restore
 
-# dotnet run --project .\benchmarks\CrossValidation.Benchmarks\CrossValidation.Benchmarks.csproj --configuration Release -- --artifacts ./generated/benchmarks --filter *.MustBenchmarks.*
+# dotnet run --project ./benchmarks/CrossValidation.Benchmarks/CrossValidation.Benchmarks.csproj --configuration Release -- --artifacts ./generated/benchmarks --filter *.MustBenchmarks.*
 
 dotnet test --configuration Release --collect:"XPlat Code Coverage;Format=lcov" --results-directory ./generated/coverage -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Exclude="[*]CrossValidation.WebApplication*,[*]CrossValidation.Resources*"
 
