970ba6f09585ce817ca048cda63977c800a90020
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-07-19 14:29:12 +0200

    Update test

--- tests/CrossValidation.Tests/Exceptions/MessageBusinessExceptionTests.cs
+++ tests/CrossValidation.Tests/Exceptions/MessageBusinessExceptionTests.cs
@@ -10,14 +10,17 @@ public class MessageBusinessExceptionTests
     [Fact]
     public void Instantiate_exception()
     {
-        var expectedMessage = "Expected message";
+        var expectedMessage = "Expected message with 1";
         var expectedStatusCode = HttpStatusCode.BadRequest;
         var expectedDetails = "Expected details";
         
-        var exception = new MessageBusinessException(expectedMessage, expectedStatusCode, expectedDetails);
+        var exception = new CustomMessageBusinessException(1);
         
         exception.Message.ShouldBe(expectedMessage);
         exception.StatusCode.ShouldBe(expectedStatusCode);
         exception.Details.ShouldBe(expectedDetails);
     }
+
+    private class CustomMessageBusinessException(int parameter)
+        : MessageBusinessException($"Expected message with {parameter}", HttpStatusCode.BadRequest, "Expected details");
 }
