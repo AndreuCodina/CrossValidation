b8bc4e3785efcc1e706e8dcb71e602a057233307
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-08-23 23:17:34 +0200

    Add inheritance to collection validators

--- src/CrossValidation/Validators/CollectionValidators/NotEmptyCollectionValidator.cs
+++ src/CrossValidation/Validators/CollectionValidators/NotEmptyCollectionValidator.cs
@@ -2,11 +2,14 @@ using CrossValidation.Exceptions;
 
 namespace CrossValidation.Validators.CollectionValidators;
 
-public class NotEmptyCollectionValidator<TField>(IEnumerable<TField> fieldValue) : Validator
+public class NotEmptyCollectionValidator<TField>(IEnumerable<TField> fieldValue)
+    : CollectionValidator<TField>(fieldValue)
 {
+    private readonly IEnumerable<TField> _fieldValue = fieldValue;
+    
     public override bool IsValid()
     {
-        return fieldValue.Any();
+        return _fieldValue.Any();
     }
 
     public override BusinessException CreateException()
