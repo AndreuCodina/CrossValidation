fe5ad51e2258c55f7082fa4621d97c5839ac0f6b
Andreu Codina <30506301+AndreuCodina@users.noreply.github.com>
2023-09-12 00:08:46 +0200

    Optimize options (#94)

--- UsefulCommands.ps1
+++ UsefulCommands.ps1
@@ -1,14 +1,14 @@
-# dotnet tool restore
+dotnet tool restore
 
-# dotnet run --project ./benchmarks/CrossValidation.Benchmarks/CrossValidation.Benchmarks.csproj --configuration Release -- --artifacts ./artifacts/benchmarks --filter *.MustBenchmarks.*
+# Run benchmark
+sudo pwsh
+dotnet run --project ./benchmarks/CrossValidation.Benchmarks/CrossValidation.Benchmarks.csproj --configuration Release -- --artifacts ./artifacts/benchmarks --filter *.MustBenchmarks.*
 
+# Get code coverage
 dotnet test --configuration Release --collect:"XPlat Code Coverage;Format=lcov" --results-directory ./artifacts/coverage -- DataCollectionRunSettings.DataCollectors.DataCollector.Configuration.Exclude="[*]CrossValidation.WebApplication*,[*]CrossValidation.Resources*"
-
 dotnet tool run reportgenerator -reports:"./artifacts/coverage/*/lcov.info" -targetdir:"./artifacts/coverage" -reporttypes:Html
-
 Start-Process ./artifacts/coverage/index.html
-
-# Remove-Item -Recurse -Force ./artifacts
+Remove-Item -Recurse -Force ./artifacts
 
 # Test NuGet packages locally
 dotnet pack --configuration Release --output ./artifacts -p:Version=0.4.1 -p:SymbolPackageFormat=snupkg --include-symbols
